417675220ec0463a0d546d4464ca4b1663dc9dda
ngimdock <ngimdockzemfack@gmail.com>
2023-07-01 11:24:22 +0100

    add AggregateRoot base

---
+++ BurberDinner.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,12 @@
+
+
+namespace BurberDinner.Domain.Common.Models;
+
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+  where TId: notnull
+{
+  protected AggregateRoot(TId id) : base(id)
+  {
+  }
+}
