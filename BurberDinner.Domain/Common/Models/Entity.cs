b2caad61954e085737eb4e27905ed3d10bf83283
ngimdock <ngimdockzemfack@gmail.com>
2023-07-01 11:21:22 +0100

    add Entity base class

---
+++ BurberDinner.Domain/Common/Models/Entity.cs
@@ -0,0 +1,39 @@
+
+
+
+namespace BurberDinner.Domain.Common.Models;
+
+
+public abstract class Entity<TId>: IEquatable<Entity<TId>>
+  where TId: notnull
+{
+
+  public TId Id { get; private set; }
+
+  protected Entity(TId id) {
+    Id = id;
+  }
+
+  public override bool Equals(object? obj)
+  {
+    return obj is Entity<TId> entity && Id.Equals(entity.Id);
+  }
+
+  public static bool operator ==(Entity<TId> left, Entity<TId> right) {
+    return Equals(left, right);
+  } 
+
+  public static bool operator !=(Entity<TId> left, Entity<TId> right) {
+    return !Equals(left, right);
+  } 
+
+  public bool Equals(Entity<TId>? other)
+  {
+    return Equals((object?)other);
+  }
+
+  public override int GetHashCode()
+  {
+    return Id.GetHashCode();
+  }
+}
